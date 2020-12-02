256b83d3b15845c4703723b4890763f1891c5b23
Jason VanBlarcum <yr410ad@gmail.com>
2020-12-02 13:20:43 -0600

    added wordpress lesson files

---
+++ fictional-university-theme/functions.php
@@ -0,0 +1,19 @@
+<?php 
+
+function watermark_files() {
+    wp_enqueue_script('main_uni_js', get_theme_file_uri('/js/scripts-bundled.js'), NULL, '1.0', true);
+    wp_enqueue_style("custom_font", "//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i");
+    wp_enqueue_style("font-awesome", "//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css");
+    wp_enqueue_style("watermark_main_styles", get_stylesheet_uri());
+}
+add_action('wp_enqueue_scripts', 'watermark_files');
+
+
+function university_features() {
+    register_nav_menu('headerMenuLocation', 'Header Menu Location');
+    register_nav_menu('footerLocationOne', 'Footer Location One');
+    register_nav_menu('footerLocationTwo', 'Footer Location Two');
+    add_theme_support('title-tag');
+}
+add_action('after_setup_theme', 'university_features');
+
