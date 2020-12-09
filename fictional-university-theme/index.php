76ae4f7163aefd369c1d306be585798c672ad545
Spyryls <yr410ad@gmail.com>
2020-12-09 12:09:28 -0600

    Made Archive files dynamic

--- fictional-university-theme/index.php
+++ fictional-university-theme/index.php
@@ -18,19 +18,22 @@ get_header();
     the_post();?>
 
       <div class="post-item">
-        <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+        <h2 class="headline headline--medium headline--post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
     
           <div class="metabox">
-            <p>Posted By: NAME on DATE in CATAGORY</p>
+            <p>Posted By: <?php the_author_posts_link(); ?> on <?php the_time("Y-m-d"); ?> in <?php echo get_the_category_list(', '); ?></p>
           </div>
 
-          <div class="generic-content">
+          <div class="generic-content"> 
             <?php the_excerpt();?>
-            <p><a href=<?php the_permalink(); ?>>Continue Reading &raquo;</a></p>
+            <p><a class="btn btn--blue" href=<?php the_permalink(); ?>>Continue Reading &raquo;</a></p>
           </div>
       </div>
 
-<?php } ?>
+<?php } 
+echo paginate_links();
+
+?>
 </div>
 
 <?php 
