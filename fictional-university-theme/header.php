fc1a420abd40fe78b4030d5019e129c581c17c04
Spyryls <yr410ad@gmail.com>
2020-12-14 15:00:03 -0600

    Connected blog to front-page; made dynamic

--- fictional-university-theme/header.php
+++ fictional-university-theme/header.php
@@ -23,7 +23,7 @@
               <li><a href="#">Programs</a></li>
               <li><a href="#">Events</a></li>
               <li><a href="#">Campuses</a></li>
-              <li <?php if (is_page('blog')) echo 'class="current-menu-item"' ?>>
+              <li <?php if (get_post_type() == 'post') echo 'class="current-menu-item"' ?>>
                 <a href="<?php echo site_url('/blog')?>">Blog</a></li>
             </ul>
           </nav>
