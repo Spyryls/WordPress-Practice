36e13bd5d5bdeb138f4547d21022c99053c3716c
Spyryls <yr410ad@gmail.com>
2021-02-26 12:11:48 -0600

    Fixed LF to CRLF issues and Files Too Long

--- fictional-university-theme/front-page.php
+++ fictional-university-theme/front-page.php
@@ -15,28 +15,49 @@
   <div class="full-width-split__inner">
     <h2 class="headline headline--small-plus t-center">Upcoming Events</h2>
 
-    <div class="event-summary">
-      <a class="event-summary__date t-center" href="#">
-        <span class="event-summary__month">Mar</span>
-        <span class="event-summary__day">25</span>
-      </a>
-      <div class="event-summary__content">
-        <h5 class="event-summary__title headline headline--tiny"><a href="#">Poetry in the 100</a></h5>
-        <p>Bring poems you&rsquo;ve wrote to the 100 building this Tuesday for an open mic and snacks. <a href="#" class="nu gray">Learn more</a></p>
-      </div>
-    </div>
-    <div class="event-summary">
-      <a class="event-summary__date t-center" href="#">
-        <span class="event-summary__month">Apr</span>
-        <span class="event-summary__day">02</span>
-      </a>
-      <div class="event-summary__content">
-        <h5 class="event-summary__title headline headline--tiny"><a href="#">Quad Picnic Party</a></h5>
-        <p>Live music, a taco truck and more can found in our third annual quad picnic day. <a href="#" class="nu gray">Learn more</a></p>
-      </div>
-    </div>
+    <?php 
+      $homePageEvents = new WP_Query(array(
+        $today = date('Ymd'),
+        'posts_per_page' => 2,
+        'post_type' => 'event',
+        'meta_key' => 'event_date',
+        'orderby' => 'meta_value_num',
+        'order' => 'ASC',
+        'meta_query' => array(
+          array(
+            'key' => 'event_date',
+            'compare' => '>=',
+            'value' => $today,
+            'type' => 'numeric'
+          )
+        )
+      ));
 
-    <p class="t-center no-margin"><a href="#" class="btn btn--blue">View All Events</a></p>
+        while($homePageEvents->have_posts()) {
+          $homePageEvents->the_post(); ?>
+          <div class="event-summary">
+            <a class="event-summary__date t-center" href="#">
+              <span class="event-summary__month"><?php
+                $eventDate = new DateTime(get_field('event_date'));
+                echo $eventDate->format('M')
+              ?></span>
+              <span class="event-summary__day"><?php echo $eventDate->format('d') ?></span>
+            </a>
+            <div class="event-summary__content">
+              <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title() ?></a></h5>
+              <p><?php 
+                  if(has_excerpt()) {
+                    echo get_the_excerpt();
+                  } else {
+                    echo wp_trim_words(get_the_content(), 18);
+                  }?>
+              <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
+            </div>
+          </div>
+        <?php }
+    ?>
+
+    <p class="t-center no-margin"><a href="<?php echo get_post_type_archive_link('event'); ?>" class="btn btn--blue">View All Events</a></p>
   </div>
 </div>
 <div class="full-width-split__two">
@@ -51,12 +72,18 @@
           $homepagePosts->the_post(); ?>
           <div class="event-summary">
           <a class="event-summary__date event-summary__date--beige t-center" href="<?php the_permalink(); ?>">
-            <span class="event-summary__month"><?php echo date("M"); ?></span>
-            <span class="event-summary__day"><?php echo date("d"); ?></span>
+            <span class="event-summary__month"><?php the_time("M"); ?></span>
+            <span class="event-summary__day"><?php the_time("d"); ?></span>
           </a>
       <div class="event-summary__content">
         <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-        <p><?php echo wp_trim_words(get_the_content(), 18); ?> <a href="<?php the_permalink(); ?>" class="nu gray">Read more</a></p>
+        <p><?php 
+            if(has_excerpt()) {
+              echo get_the_excerpt();
+            } else {
+              echo wp_trim_words(get_the_content(), 18);
+            }?>
+          <a href="<?php the_permalink(); ?>" class="nu gray">Read more</a></p>
       </div>
     </div>
       <?php } wp_reset_postdata();
@@ -106,4 +133,3 @@
 
 
 <?php get_footer(); ?>
-
--- fictional-university-theme/functions.php
+++ fictional-university-theme/functions.php
@@ -8,9 +8,9 @@ function watermark_files() {
     if(strstr($_SERVER['SERVER_NAME'], 'fictional-university.local')){
         wp_enqueue_script('main-uni-js', 'http://localhost:3000/bundled.js', NULL, '1.0', true);
     } else {
-        wp_enqueue_script("our-vendor-js", get_theme_file_uri('/bundled-assets/vendor.js'));
-        wp_enqueue_script("main-university-js", get_theme_file_uri('/bundled-assets/scripts.js'));
-        wp_enqueue_style("our-main-styles", get_theme_file_uri('/bundled-assets/style.css'));
+        wp_enqueue_script("our-vendor-js", get_theme_file_uri('/bundled-assets/vendors~scripts.8c97d901916ad616a264.js'));
+        wp_enqueue_script("main-university-js", get_theme_file_uri('/bundled-assets/scripts.1f030ffd450d191a9503.js'));
+        wp_enqueue_style("our-main-styles", get_theme_file_uri('/bundled-assets/styles.1f030ffd450d191a9503.css'));
     }
 }
 add_action('wp_enqueue_scripts', 'watermark_files');
@@ -21,3 +21,21 @@ function university_features() {
 }
 add_action('after_setup_theme', 'university_features');
 
+function university_adjust_queries($query) {
+    if (!is_admin() AND is_post_type_archive('event') AND $query->is_main_query()) {
+        $today = date('Ymd');
+        $query->set('meta_key', 'event_date');
+        $query->set('orderby', 'meta_value_num');
+        $query->set('order', 'ASC');
+        $query->set('meta_query', array(
+            array(
+              'key' => 'event_date',
+              'compare' => '>=',
+              'value' => $today,
+              'type' => 'numeric'
+            )
+            ));
+    }
+}
+
+add_action('pre_get_posts', 'university_adjust_queries');
